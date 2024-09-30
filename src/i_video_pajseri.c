bc665f6d3828b77491f2d151afca04f68614662b
Aleksa Vuckovic <aleksa@vuckovic.cc>
2024-09-30 22:38:10 +0200

    keyboard support cyclone

--- src/i_video_pajseri.c
+++ src/i_video_pajseri.c
@@ -15,7 +15,6 @@ void I_InitGraphics(void)
 
 void I_ShutdownGraphics(void)
 {
-	end = true;
 	cleanup();
 }
 
@@ -69,90 +68,132 @@ void I_StartFrame()
 {
 }
 
-#ifndef __riscv
+#define KEY_ADDR 0x30000000UL
+#define ESC 0x80
+#define SPACE 0x40
+#define D 0x20
+#define S 0x10
+#define A 0x08
+#define W 0x04
+#define SHIFT 0x02
+#define CTRL 0x01
+
+uint8_t kb_status = 0;
+
 void I_StartTic()
 {
-	if (end) {
-		I_ShutdownGraphics();
+	poll_keyboard();
+	volatile uint8_t* keyboard = (uint8_t*)KEY_ADDR;
+	uint8_t key = *keyboard;
+	if (ESC & key) {
+		event_t event;
+		event.type = ev_keyup;
+		event.data1 = KEY_PAUSE;
+		D_PostEvent(&event);
+		kb_status |= ESC;
+	} else if (kb_status & ESC) {
+		event_t event;
+		event.type = ev_keydown;
+		event.data1 = KEY_PAUSE;
+		D_PostEvent(&event);
+		kb_status &= (uint8_t)~ESC;
 	}
 
-	poll_keyboard();
-	if (!change_ack) {
-		int rc = 0;
-
-		switch (key_changed) {
-		case LEFT:
-			rc = KEY_LEFTARROW;
-			break;
-		case RIGHT:
-			rc = KEY_RIGHTARROW;
-			break;
-		case UP:
-			rc = KEY_UPARROW;
-			break;
-		case DOWN:
-			rc = KEY_DOWNARROW;
-			break;
-		case SPACE:
-			rc = ' ';
-			break;
-		case CTRL:
-			rc = KEY_RCTRL;
-			break;
-		default:
-			break;
-		}
-
+	if (SPACE & key) {
 		event_t event;
-		event.type = key_pressed ? ev_keydown : ev_keyup;
-		event.data1 = rc;
+		event.type = ev_keyup;
+		event.data1 = ' ';
 		D_PostEvent(&event);
+		kb_status |= SPACE;
+	} else if (kb_status & SPACE) {
+		event_t event;
+		event.type = ev_keydown;
+		event.data1 = ' ';
+		D_PostEvent(&event);
+		kb_status &= (uint8_t)~SPACE;
+	}
 
-		change_ack = true;
-		//printf("key event: %d %d\n", event.type, rc);
+	if (D & key) {
+		event_t event;
+		event.type = ev_keyup;
+		event.data1 = KEY_RIGHTARROW;
+		D_PostEvent(&event);
+		kb_status |= D;
+	} else if (kb_status & D) {
+		event_t event;
+		event.type = ev_keydown;
+		event.data1 = KEY_RIGHTARROW;
+		D_PostEvent(&event);
+		kb_status &= (uint8_t)~D;
+	}
+
+	if (S & key) {
+		event_t event;
+		event.type = ev_keyup;
+		event.data1 = KEY_DOWNARROW;
+		D_PostEvent(&event);
+		kb_status |= S;
+	} else if (kb_status & S) {
+		event_t event;
+		event.type = ev_keydown;
+		event.data1 = KEY_DOWNARROW;
+		D_PostEvent(&event);
+		kb_status &= (uint8_t)~S;
+	}
+
+	if (A & key) {
+		event_t event;
+		event.type = ev_keyup;
+		event.data1 = KEY_LEFTARROW;
+		D_PostEvent(&event);
+		kb_status |= A;
+	} else if (kb_status & A) {
+		event_t event;
+		event.type = ev_keydown;
+		event.data1 = KEY_LEFTARROW;
+		D_PostEvent(&event);
+		kb_status &= (uint8_t)~A;
+	}
+
+	if (W & key) {
+		event_t event;
+		event.type = ev_keyup;
+		event.data1 = KEY_UPARROW;
+		D_PostEvent(&event);
+		kb_status |= W;
+	} else if (kb_status & W) {
+		event_t event;
+		event.type = ev_keydown;
+		event.data1 = KEY_UPARROW;
+		D_PostEvent(&event);
+		kb_status &= (uint8_t)~W;
+	}
+
+	if (SHIFT & key) {
+		event_t event;
+		event.type = ev_keyup;
+		event.data1 = KEY_PAUSE;
+		D_PostEvent(&event);
+		kb_status |= SHIFT;
+	} else if (kb_status & SHIFT) {
+		event_t event;
+		event.type = ev_keydown;
+		event.data1 = KEY_PAUSE;
+		D_PostEvent(&event);
+		kb_status &= (uint8_t)~SHIFT;
+	}
+
+	if (CTRL & key) {
+		event_t event;
+		event.type = ev_keyup;
+		event.data1 = KEY_RCTRL;
+		D_PostEvent(&event);
+		kb_status |= CTRL;
+	} else if (kb_status & CTRL) {
+		event_t event;
+		event.type = ev_keydown;
+		event.data1 = KEY_RCTRL;
+		D_PostEvent(&event);
+		kb_status &= (uint8_t)~CTRL;
 	}
 }
-#else
-void I_StartTic()
-{
-	if (*end) {
-		I_ShutdownGraphics();
-	}
-
-	poll_keyboard();
-	if (!*change_ack) {
-		int rc = 0;
-
-		switch (*key_changed) {
-		case LEFT:
-			rc = KEY_LEFTARROW;
-			break;
-		case RIGHT:
-			rc = KEY_RIGHTARROW;
-			break;
-		case UP:
-			rc = KEY_UPARROW;
-			break;
-		case DOWN:
-			rc = KEY_DOWNARROW;
-			break;
-		case SPACE:
-			rc = ' ';
-			break;
-		case CTRL:
-			rc = KEY_RCTRL;
-			break;
-		default:
-			break;
-		}
-
-		event_t event;
-		event.type = *key_pressed ? ev_keydown : ev_keyup;
-		event.data1 = rc;
-		D_PostEvent(&event);
-
-		*change_ack = true;
-		//printf("key event: %d %d\n", event.type, rc);
-	}
-}
-#endif
